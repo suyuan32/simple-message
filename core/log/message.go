cd89c394ce7a697281988d573b1df4e0b22026f3
Ryan SU <1137661202@qq.com>
2022-10-30 11:58:19 +0800

    feat: core and file manager message

---
+++ core/log/message.go
@@ -0,0 +1,14 @@
+package log
+
+const (
+	CreateSuccess     string = "create successfully"
+	CreateFailed      string = "create failed"
+	DeleteSuccess     string = "delete successful"
+	DeleteFailed      string = "delete failed"
+	UpdateSuccess     string = "update successful"
+	UpdateFailed      string = "update failed"
+	TargetNotFound    string = "target cannot be found"
+	DatabaseError     string = "database error"
+	RedisError        string = "redis error"
+	OperationNotAllow string = "Operation does not allow"
+)
